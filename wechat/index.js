4fd0710eee29621b93379b25155c16c23a823be4
何坦宏 <hetanhong@mofanghr.com>
2017-03-07 13:40:45 +0800

    oauth

--- wechat/index.js
+++ wechat/index.js
@@ -16,30 +16,30 @@ var config = {
 };
 
 router.use(express.query());
-router.use('/wechat',wechat(config, function (req, res, next) {
-     new OAuth(appid, secret, function (openid, callback) {
+function uauth() {
+    new OAuth(appid, secret, function (openid, callback) {
         // 传入一个根据openid获取对应的全局token的方法
         // 在getUser时会通过该方法来获取token
-         console.log(openid);
+        console.log(openid);
         fs.readFile(openid +':access_token.txt', 'utf8', function (err, txt) {
             if (err) {return callback(err);}
             callback(null, JSON.parse(txt));
         });
-        next();
+
     }, function (openid, token, callback) {
         // 请将token存储到全局，跨进程、跨机器级别的全局，比如写到数据库、redis等
         // 这样才能在cluster模式及多机情况下使用，以下为写入到文件的示例
         // 持久化时请注意，每个openid都对应一个唯一的token!
-         console.log(openid);
-         console.log(token);
+        console.log(openid);
+        console.log(token);
         fs.writeFile(openid + ':access_token.txt', JSON.stringify(token), callback);
-         next();
     });
-}))
+}
 router.use('/wechat', wechat(config, function (req, res, next) {
     // 微信输入信息都在req.weixin上
     var message = req.weixin;
     console.log(message);
+    uauth();
     if(!message.Content){
         res.reply({
             content: 'what are you say',
